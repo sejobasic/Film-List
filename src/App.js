1b0a53d524b2f5dda710b97fe5fe78b7aa66cb06
Sejo Basic <sejoob@gmail.com>
2022-08-02 16:38:49 -0700

    Basic Router set up for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+// Page Components
+
+import Home from './pages/home/Home';
+import Search from './pages/search/Search';
+import Create from './pages/create/Create';
+import Film from './pages/film/Film';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' >
+            <Home />
+          </Route>
+          <Route path='/search'>
+            <Search />
+          </Route>
+          <Route path='/create'>
+            <Create />
+          </Route>
+          <Route path='/films/:id' >
+            <Film />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
