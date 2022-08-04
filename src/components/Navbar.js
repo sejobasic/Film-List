187a78c8afce7e7fd72fd3e4ac9f054f91b5a78a
Sejo Basic <sejoob@gmail.com>
2022-08-03 20:18:18 -0700

    Created useTheme hook to reduce clutter/boilerplate

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
 import SearchBar from './SearchBar'
+import { ThemeContext } from '../context/ThemeContext'
 
 
 function Navbar() {
+  const { color } = useContext(ThemeContext)
+
   return (
-    <div className='navbar'>
+    <div className='navbar' style={{ background: color}}>
       <nav>
         <Link exact to='./' className='brand'>
           <h1>Film List</h1>
