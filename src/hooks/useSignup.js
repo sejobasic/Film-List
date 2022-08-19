5f33b6cc4dc70e50b99d8fcd965aa4eaf0c88afb
Sejo Basic <sejoob@gmail.com>
2022-08-19 16:41:33 -0700

    Custom hook for singup functionality

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { auth } from '../firebase/config'
+
+export const useSignup = () => {
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  // signup function to invoke after user clicks submit
+  const signup = async (email, displayName, password) => {
+    setError(null)
+    setLoading(true)
+
+    try {
+      // signup user
+      // store email and password in firebase
+      const resp = await auth.createUserWithEmailAndPassword(email, password)
+      
+
+      // if resp is invalid throw error message
+      if (!resp) {
+        throw new Error('Could not complete signup at this time, please try again later')
+      }
+
+      // take the user that just signed up and update their profile so they have a display name
+      // add display name to user
+      await resp.user.updateProfile({ displayName })
+
+      setLoading(false)
+      setError(null)
+
+    }
+    catch (err) {
+      console.log(err.message)
+      setError(err.message)
+      setLoading(false)
+    }
+  }
+
+  return { error, loading, signup }
+}
