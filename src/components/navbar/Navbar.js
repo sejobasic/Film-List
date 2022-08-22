bc0e55297868fb74396eab62c0b680f8fdeb82b6
Sejo Basic <sejoob@gmail.com>
2022-08-22 15:28:22 -0700

    Update AuthContext to communicate with firebase to check if user is logged in or not before rendering page content

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,10 +6,12 @@ import { useTheme } from '../../hooks/useTheme'
 import { useLogout } from '../../hooks/useLogout'
 import logo from '../../assets/logo.png'
 import logoutIcon from '../../assets/logout-icon.svg'
+import { useAuthContext } from '../../hooks/useAuthContext'
 
 function Navbar() {
   const { color } = useTheme()
   const { logout } = useLogout()
+  const { user } = useAuthContext()
 
   return (
     <div className='navbar' style={{ background: color }}>
@@ -17,16 +19,22 @@ function Navbar() {
         <Link exact to='/' className='brand'>
           <img className='logo' src={logo} alt='Film List Logo' />
         </Link>
-        <SearchBar />
-        <Link className='add-film' to='/create?action=create'>
-          Add Film
-        </Link>
-        <img 
-          className='logout' 
-          src={logoutIcon}
-          onClick={logout}
-          alt='logout icon' 
-        />
+        {/* Render elements only when user is logged in */}
+        {user && (
+          <>
+            {/* <SearchBar /> */}
+            <Link className='add-film' to='/create?action=create'>
+              Add Film
+            </Link>
+            <p>logged in as: {user.displayName}</p>
+            <img 
+              className='logout' 
+              src={logoutIcon}
+              onClick={logout}
+              alt='logout icon' 
+            />
+          </>
+        )}
       </nav>
     </div>
   )
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,7 @@
 // store user object in global state when user is logged in
 
-import { createContext, useReducer } from 'react'
+import { createContext, useEffect, useReducer } from 'react'
+import { auth } from '../firebase/config'
 
 export const AuthContext = createContext()
 
@@ -14,6 +15,8 @@ export const authReducer = (state, action) => {
       return { ...state, user: action.payload }
     case 'LOGOUT':
       return { ...state, user: null}
+    case 'AUTH_IS_READY':
+      return { ...state, user: action.payload, authIsReady: true}
     default:
       return state
   }
@@ -22,7 +25,18 @@ export const authReducer = (state, action) => {
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
+    authIsReady: false
   })
+
+  // check when user logs in or out
+  // func will run anytime user auth state changes
+  useEffect(() => {
+    // communicate with firebase to tell us whenever theres some kind of change in auth status and when there is we want to fire this function
+    const unsub = auth.onAuthStateChanged((user) => {
+      dispatch({ type: 'AUTH_IS_READY', payload: user})
+      unsub()
+    })
+  }, [])
 
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
